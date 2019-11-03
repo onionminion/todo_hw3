0d475a89782b87949a17b114db9454991db53b54
Sooyeon Kim <sooyeon.kim.2@stonybrook.edu>
2019-11-02 23:48:51 -0400

    initialized firebase

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -5,15 +5,15 @@ import 'firebase/auth';
 // THIS IS USED TO INITIALIZE THE firebase OBJECT
 // PUT YOUR FIREBASE PROJECT CONFIG STUFF HERE
 var firebaseConfig = {
-    apiKey: "AIzaSyCJxkqx-6PMJrZ7ACkrgbO55b5wmJdop1Y",
-    authDomain: "todo-rrf-316.firebaseapp.com",
-    databaseURL: "https://todo-rrf-316.firebaseio.com",
-    projectId: "todo-rrf-316",
-    storageBucket: "todo-rrf-316.appspot.com",
-    messagingSenderId: "892398996038",
-    appId: "1:892398996038:web:1fb9157fc6c5d266e01847",
-    measurementId: "G-TEGQB3MZ23"
-};
+    apiKey: "AIzaSyBFtGhdpvnnUTmcx8xNv5n8reL6sp0U48Y",
+    authDomain: "sk-todo-hw3.firebaseapp.com",
+    databaseURL: "https://sk-todo-hw3.firebaseio.com",
+    projectId: "sk-todo-hw3",
+    storageBucket: "sk-todo-hw3.appspot.com",
+    messagingSenderId: "990680882115",
+    appId: "1:990680882115:web:1980f048833d03103dfb76",
+    measurementId: "G-5X87VZ7Y72"
+  };
 firebase.initializeApp(firebaseConfig);
 
 // NOW THE firebase OBJECT CAN BE CONNECTED TO THE STORE
